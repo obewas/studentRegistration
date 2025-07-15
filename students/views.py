91711a5757764fb0ecadcfa171dd972129657e60
obewas <obewas1202@gmail.com>
2025-07-15 10:04:13 +0300

    added permissions

--- students/views.py
+++ students/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
